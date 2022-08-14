67abdf8acc2c57d704b549f1ed607aba5b8510a1
Matthew R. Dangle <matthewrdangle@gmail.com>
2022-08-14 00:38:53 -0400

    Add files via upload

---
+++ src/lib/js/utility.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    const nav_element = document.getElementById('nav');
+    nav_element.classList.toggle('show');
+}
+
+function submitContact(e) {
+    e.preventDefault();
+}
